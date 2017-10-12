02ea48ba27ef487562b0d5f26d278fb5be6626d3
Renato Mintz <remintz@gmail.com>
2017-10-12 16:40:14 -0300

    Improve custom nodes

--- node-red-files/nodes/buildmsg.js
+++ node-red-files/nodes/buildmsg.js
@@ -1,24 +1,38 @@
 module.exports = function (RED) {
-
-   function buildMsgNode(config) {
-      RED.nodes.createNode(this, config);
-      var node = this;
-      var context = this.context();
-      var globalContext = this.context().global;
-
-      this.on('input', function (msg) {
-	 var topic = 'rpdlite/' + config.gateway + '/' + config.msgtype + '/' + config.porttype + '/' + config.address;
-         var payload = {
-            device: config.gateway,
-            type: config.porttype,
-            address: config.address,
-            value: msg.payload
-            };
-         msg.topic = topic;
-         msg.payload = payload;
-         node.send(msg)
-      });
+   
+      function buildMsgNode(config) {
+         RED.nodes.createNode(this, config);
+         var node = this;
+         var context = this.context();
+         var globalContext = this.context().global;
+   
+         this.on('input', function (msg) {
+            var device = config.gateway;
+            if (device.trim().length == 0) {
+               device = globalContext.get("DEFAULT_EDGE_CONTROLLER");
+            }
+            if (device) {
+               device = device.toLowerCase();
+            }
+            var address = config.address;
+            if (address.trim().length == 0) {
+               address = msg.topic;
+            }
+            address = address.toLowerCase();
+            var porttype = config.porttype.toLowerCase();
+       var topic = 'rpdlite/' + device + '/' + config.msgtype + '/' + porttype + '/' + address;
+            var payload = {
+               device: device,
+               type: porttype,
+               address: address,
+               value: msg.payload
+               };
+            msg.topic = topic;
+            msg.payload = payload;
+            node.send(msg)
+         });
+      }
+      RED.nodes.registerType("build-msg", buildMsgNode);
    }
-   RED.nodes.registerType("build-msg", buildMsgNode);
-}
-
+   
+   
