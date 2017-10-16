e0fdfb49ffc4c7b2d2e4d873e51675b6625e5056
Renato Mintz <remintz@gmail.com>
2017-10-16 17:25:22 -0200

    Added MongoDB

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM nodered/node-red-docker
 USER root
 RUN npm install node-red-dashboard
 RUN npm install node-red-node-twilio
+RUN npm install node-red-node-mongodb
 EXPOSE 1880
 CMD ["npm", "start", "--", "--userDir", "/data"]
--- run_docker_server.sh
+++ run_docker_server.sh
@@ -4,15 +4,18 @@ PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 VERBOSE=
 IMAGE=dockerhub.accenture.com/renato.mintz/rpdlite-server
 MAPPED_DIR=/home/admin/node-red-user-data
+MAPPED_DIR_MONGO=/home/admin/dbdata
 CONTAINER_NAME=node-red-container
 HOSTNAME=$(hostname -f)
+MONGO_CONTAINER_NAME=mongodb-container
+
 
 function usage
 {
     echo "usage: run_docker.sh <options>"
     echo "options:"
     echo "     -i <image> optional - default is $IMAGE"
-    echo "     -d <mapped dir> optional - default is $MAPPED_DIR"
+    echo "     -d <node red mapped dir> optional - default is $MAPPED_DIR"
     echo "     -v verbose output"
     echo "     -h prints this message"
 }
@@ -55,8 +58,15 @@ fi
 
 if [ ! "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
    echo $(date -u) "Container $CONTAINER_NAME not found"
-   docker run -d -p 80:1880 -v $MAPPED_DIR:/data --hostname=$HOSTNAME --name $CONTAINER_NAME $IMAGE
+   docker run -d -p 27017:27017 -v $MAPPED_DIR_MONGO:/data/db --name $MONGO_CONTAINER_NAME mongo
+   docker run -d -p 80:1880 -v $MAPPED_DIR:/data --link $MONGO_CONTAINER_NAME:mongo --hostname=$HOSTNAME --name $CONTAINER_NAME $IMAGE
 else
+   if [ "$(docker ps -aq -f status=running -f name=$MONGO_CONTAINER_NAME)" ]; then
+      echo $(date -u) "Container $MONGO_CONTAINER_NAME already running"
+   else
+      echo $(date -u) "Starting container $MONGO_CONTAINER_NAME"
+      docker start $MONGO_CONTAINER_NAME
+   fi
    if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
       echo $(date -u) "Container $CONTAINER_NAME already running"
    else
